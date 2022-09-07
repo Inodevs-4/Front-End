b150a6a531e9b61b2da5093e0ea756b972f249f8
Richard Rafael Sacramento Soares <richard.rafael.soares@gmail.com>
2022-09-07 15:23:17 -0300

    começo do login

---
+++ 2RP/src/pages/index.tsx
@@ -0,0 +1,40 @@
+import { useState} from "react";
+import './style.css';
+
+
+
+export const Login = () =>{
+    const [matricula,SetEmail] = useState('');
+    const [password,SetPassword] = useState('');
+
+
+    return(
+        <div className="container">
+           <h1 className='h1'>Entrar</h1>
+            <div className="containerbt">
+
+            <input type="text" 
+            className='btn botao' 
+            placeholder='Matricula '
+            value={matricula}
+           />
+            <input type="Password" 
+            className='btn botao' 
+            placeholder='Senha '
+            value={password}
+            />
+            <input type="submit" 
+            className="btnsubmit botao"
+             />
+            {/* <input type="checkbox" 
+            name="remember" 
+            id="remember"
+             /> 
+             <p>Lembre de mim</p> */}
+            
+         
+            </div>
+        </div>
+    );
+    }
+    export default Login
