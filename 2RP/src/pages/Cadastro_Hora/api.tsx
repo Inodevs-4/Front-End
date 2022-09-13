a61d68f6ef31409fdca84ab13db4b51d6c604932
Richard Rafael Sacramento Soares <richard.rafael.soares@gmail.com>
2022-09-12 23:10:18 -0300

    começo cadastro

---
+++ 2RP/src/pages/Cadastro_Hora/api.tsx
@@ -0,0 +1,24 @@
+import './style.css';
+
+
+
+export const  fakeApi =  () =>{
+    // new Promise((resolve) => {
+    //         setTimeout(() => {
+    //           resolve(cb());
+    //         }, delay);
+    //       });
+    }
+    export default fakeApi
+
+
+
+// export const fakeApi = () =>{
+//     return(<div> <h1>123</h1></div>);
+// //   new Promise((resolve) => {
+// //     setTimeout(() => {
+// //       resolve(cb());
+// //     }, delay);
+// //   });
+
+// export default fakeApi
