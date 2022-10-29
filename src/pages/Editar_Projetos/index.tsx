dd1adabdc143c7bef1bf231a9035aef0898f4fd8
Richard Rafael Sacramento Soares <richard.rafael.soares@gmail.com>
2022-10-29 14:33:17 -0300

    Update index.tsx

--- src/pages/Editar_Projetos/index.tsx
+++ src/pages/Editar_Projetos/index.tsx
@@ -88,12 +88,22 @@ return(
 
              <div className="col-md">
                      <div className="form-floating">
-                         <input type="tel" className="form-control" id="floatingInputGrid" onChange={handleChange} disabled={isDisabled}value={projeto?.nome} name='telefone'/>
+                         <input type="tel" className="form-control" id="floatingInputGrid" onChange={handleChange} disabled={isDisabled} value={projeto?.nome} name='nome'/>
                          <label htmlFor="floatingInputGrid">Nome</label>
                      </div>
                  </div>
 
                  <div className="col-md">
+                     <div className="form-floating">
+                         <input type="tel" className="form-control" id="floatingInputGrid" onChange={handleChange} disabled={isDisabled} value={projeto?.cliente} name='cliente'/>
+                         <label htmlFor="floatingInputGrid">Cliente</label>
+                     </div>
+                 </div>
+
+
+
+
+                 {/* <div className="col-md">
                      <div className="form-floating">
                          <select  className="form-select" id="floatingInputGrid" onChange={handleSelect}disabled={isDisabled} value={projeto?.cliente} name="modalidade">
                          <option value="hora extra">1</option>
@@ -101,7 +111,7 @@ return(
                          </select>
                          <label htmlFor="floatingInputGrid">Cliente</label>
                      </div>
-                 </div>  
+                 </div>   */}
 
                 
 
@@ -110,9 +120,10 @@ return(
              <div className="row g-2">
                  <div className="col-md">
                      <div className="form-floating">
-                         <select  className="form-select" id="floatingInputGrid" onChange={handleSelect}disabled={isDisabled} value={projeto?.cr} name="modalidade">
-                         <option value="hora extra">1</option>
-                        <option value="sobreaviso">2</option>
+                         <select  className="form-select" id="floatingInputGrid" onChange={handleSelect} disabled={isDisabled} value={projeto?.cr} name="cr">
+                         <option value="tribo">Tribo</option>
+                         <option value="squad">Squad</option>
+                         <option value="departemento">Departamento</option>
                          </select>
                          <label htmlFor="floatingInputGrid">CR</label>
                      </div>
@@ -121,9 +132,9 @@ return(
                 
                  <div className="col-md">                        
              <div className="form-floating">
-                         <select className="form-select" aria-label="Disabled select example" disabled={isDisabled} onChange={handleSelect} value={projeto?.status} name="acionado" >
-                         <option value="nao">Ativo</option>
-                         <option value="sim">Inativo</option>
+                         <select className="form-select" aria-label="Disabled select example"  onChange={handleSelect} disabled={isDisabled} value={projeto?.status} name="status" >
+                         <option value="ativo">Ativo</option>
+                         <option value="inativo">Inativo</option>
                          </select>
                          <label htmlFor="floatingInputGrid">Status</label>
                      </div>
