48828048f1dab2397e63f97539520b72a2c819d2
Richard Rafael Sacramento Soares <richard.rafael.soares@gmail.com>
2022-11-05 13:25:27 -0300

    Update index.tsx

--- src/pages/Editar_Verba/index.tsx
+++ src/pages/Editar_Verba/index.tsx
@@ -69,46 +69,46 @@ export const Editar_Verbas = () =>{
                 </div>
                 <div className="form-group ">
                     <label className="titulo"htmlFor="exampleFormControlInput1">Número</label>
-                    <input type="number" className="form-control" onChange={handleChange} id="numero" value={verba?.numero} />
+                    <input type="number" className="form-control" onChange={handleChange} disabled={isDisabled} id="numero" value={verba?.numero} />
                 </div>
                 <div className="form-group ">
                     <label className="titulo"htmlFor="exampleFormControlInput1">Adicional</label>
-                    <input type="number" className="form-control" onChange={handleChange} id="adicional" value={verba?.adicional} placeholder='0%' />
+                    <input type="number" className="form-control" onChange={handleChange} id="adicional"  disabled={isDisabled} value={verba?.adicional} placeholder='0%' />
                 </div>
             </div>
             <div className="linha">
                 <div className="form-group ">
                     <label className="titulo"htmlFor="exampleFormControlInput1">Início</label>
-                    <input type="time" className="form-control" id="Inicio" onChange={handleChange}  value={formatarHoraInput(String(verba?.inicio))} name="inicio"  />
+                    <input type="time" className="form-control" id="Inicio" onChange={handleChange} disabled={isDisabled}  value={formatarHoraInput(String(verba?.inicio))} name="inicio"  />
                 </div>
                 <div className="form-group ">
                     <label className="titulo"htmlFor="exampleFormControlInput1">Fim</label>
-                    <input type="time" className="form-control" onChange={handleChange} value={formatarHoraInput(String(verba?.fim))} id="fim" name="fim" />
+                    <input type="time" className="form-control" onChange={handleChange} disabled={isDisabled} value={formatarHoraInput(String(verba?.fim))} id="fim" name="fim" />
                 </div>
             </div>
             <div className="linha">
                 <p>Eventos:</p> 
                 <div className="form-check">
-                    <input className="form-check-input" onChange={handleSelect} type="checkbox" id="gridCheck"/>
+                    <input className="form-check-input" onChange={handleSelect}disabled={isDisabled}  type="checkbox" id="gridCheck"/>
                     <label className="form-check-label" htmlFor="gridCheck">
                         Feriado
                     </label>
                 </div>
                 <p>Período Semanal:</p>
                 <div className="form-check">
-                    <input className="form-check-input" onChange={handleSelect} type="radio" name="gridRadios" id="gridRadios1" value="option1" />
+                    <input className="form-check-input" onChange={handleSelect} disabled={isDisabled} type="radio" name="gridRadios" id="gridRadios1" value="option1" />
                     <label className="form-check-label" htmlFor="gridRadios1">
                         Segunda à Sexta
                     </label>
                 </div>
                 <div className="form-check">
-                    <input className="form-check-input" onChange={handleSelect} type="radio" name="gridRadios" id="gridRadios1" value="option2" />
+                    <input className="form-check-input" onChange={handleSelect} disabled={isDisabled} type="radio" name="gridRadios" id="gridRadios1" value="option2" />
                     <label className="form-check-label" htmlFor="gridRadios1">
                         Sábado
                     </label>
                 </div>
                 <div className="form-check">
-                    <input className="form-check-input"  onChange={handleSelect} type="radio" name="gridRadios" id="gridRadios2" value="option3"/>
+                    <input className="form-check-input"  onChange={handleSelect} disabled={isDisabled} type="radio" name="gridRadios" id="gridRadios2" value="option3"/>
                     <label className="form-check-label"htmlFor="gridRadios2">
                         Domingo
                     </label>
@@ -118,8 +118,8 @@ export const Editar_Verbas = () =>{
             
             <div className="bottons">
                 <div className="form-group">
-                    <label className='titulo' htmlFor="exampleFormControlInput1">Cálculo(R$)</label>
-                    <input className="form-control" type="text" onChange={handleChange} placeholder="00,00" id='exampleFormControlInput1' readOnly/>
+                    <label className='titulo'  htmlFor="exampleFormControlInput1">Cálculo(R$)</label>
+                    <input className="form-control" type="text" disabled={isDisabled} onChange={handleChange} placeholder="00,00" id='exampleFormControlInput1' readOnly/>
                 </div>
                 <div className='alteracao' hidden={isVisible}>
                     <button className='btn btn-danger' onClick={cancelar}>Cancelar</button>
