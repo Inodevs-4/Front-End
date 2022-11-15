28cdb97f0eab54bc92e5a9b3adc2b08858bd22b0
FelipeASousa <feaugusto893@gmail.com>
2022-11-15 12:55:45 -0300

    Quadro de horas extras (dashboard)

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,7 +8,9 @@ import { meusLancamentos, todosLancamentos } from '../../hooks/Lancamento';
 import { Lancamento } from '../../types/Types';
 import { AuthContext } from '../../login/AuthContext';
 import "./stylesDashboard.css"
-import { formatarDataHora } from '../../functions/formatar';
+import { formatarDataHora, formatarHora, formatarInicial } from '../../functions/formatar';
+import { MdMoreTime } from 'react-icons/md';
+
 export const Dashboard = () => {
     const auth = useContext(AuthContext)
     const [mLancamentos, setMLancamentos] = useState<Lancamento[]>([])
@@ -30,13 +32,20 @@ export const Dashboard = () => {
         <body>
             <Navbar/>
             <div className="hora">
+            <h4>Quadro de Horas extras e Sobre avisos</h4>
+            <hr />
             <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1" >
                     <Row>
                         <Col sm={4}>
                         <ListGroup className='scroll'>
+                            <ListGroup.Item action href='#link1'>
+                                <p>Default</p>
+                            </ListGroup.Item>
                             {mLancamentos.map((Lancamentos) => (
                                 <ListGroup.Item action href={'#' + String(Lancamentos?.id)} key={Lancamentos?.id}>
-                                    Hora Extra #{Lancamentos?.id}
+                                {Lancamentos?.modalidade?.includes("hora extra") ? <h5>Hora Extra</h5> : <h5>Sobre aviso</h5>}
+                                <hr />
+                                <p className='horasobre'> {formatarDataHora(String(Lancamentos?.data_inicio))}</p>
                                 </ListGroup.Item>
                             ))}
                         </ListGroup>
@@ -44,13 +53,31 @@ export const Dashboard = () => {
 
                         <Col sm={8}>
                         <Tab.Content>
-
+                            <Tab.Pane eventKey='#link1'>
+                                <MdMoreTime size={200} className="icone"/>    
+                            </Tab.Pane>
                             {mLancamentos.map((Lancamentos) => (
                                  <Tab.Pane eventKey={'#' + String(Lancamentos?.id)} key={Lancamentos?.id}>
                                     {Lancamentos?.status?.includes("pendente") ? 
                                         <div>
-                                            <p>Gestor: { Lancamentos?.gestor?.nome}</p>
-                                            <p>Projeto: { Lancamentos?.projeto?.nome}</p>
+                                            {Lancamentos?.modalidade?.includes("hora extra") ? <span>Hora Extra</span> : <span>Sobre aviso</span>}
+                                            <p className="dados"><strong>Gestor:</strong> { Lancamentos?.gestor?.nome}</p>
+                                            <p className="dados"><strong>Projeto:</strong> { Lancamentos?.projeto?.nome}</p>
+                                            <p className="dados"><strong>Status:</strong> { Lancamentos?.status}</p>
+                                            <hr />
+                                            <div>
+                                                <p className="dados"><strong>Data Início:</strong> {formatarDataHora(String(Lancamentos?.data_inicio))}</p>
+                                                <p className="dados"><strong>Data Fim:</strong> { formatarDataHora(String(Lancamentos?.data_fim))}</p>
+                                            </div>
+                                            <hr />
+                                            <p className="dados"><strong>Observações:</strong> { Lancamentos?.observacoes}</p>
+                                            <hr />
+                                            {!Lancamentos.data_inicio2 ? <p></p> : 
+                                            <div>
+                                                <p className="dados"><strong>2ª Data Inicio:</strong> {formatarDataHora(String(Lancamentos?.data_inicio2))}</p>
+                                                <p className="dados"><strong>2ª Data Fim:</strong> { formatarDataHora(String(Lancamentos?.data_fim2))}</p>
+                                            </div>
+                                            }
                                         </div>
                                     : <p>Nao pendente</p> }
                                 </Tab.Pane>
