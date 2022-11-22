c19957451d9fbf30b2103ace1af031e8f8ae7694
Richard Rafael Sacramento Soares <richard.rafael.soares@gmail.com>
2022-11-22 20:10:02 -0300

    add_link img

--- src/components/menu/Navbar.tsx
+++ src/components/menu/Navbar.tsx
@@ -1,6 +1,7 @@
 import * as S from './styles';
 
 import Burger from './Burger';
+import { NavLink } from 'react-router-dom';
 
 type Props = {
   children?: ChildNode;
@@ -10,7 +11,13 @@ export default function Navbar(props: Props) {
   return (
     <>
       <S.Nav>
-        <S.Logo src={"https://www.2rpnet.com.br/assets/images/2rp-net.svg"} alt='Gustavo Scarpim' />
+      <NavLink
+      to='/home'
+    >
+       <S.Logo src={"https://www.2rpnet.com.br/assets/images/2rp-net.svg"} alt='Gustavo Scarpim' />
+      
+    </NavLink>
+      
       </S.Nav>
       <Burger />
       {props.children}
